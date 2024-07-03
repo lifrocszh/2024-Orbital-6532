45b6c95548b9fea7eb98f01ee9551ab44eec1f86
lifrocszh <wangziheng516@gmail.com>
2024-07-04 01:06:32 +0800

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -66,7 +66,7 @@ class _HomeState extends State<HomePage> {
         );
         setState(() {});
       },
-      child: const Text('Make An Announcement'),
+      child: const Text('Make An Announcement!'),
       style: ButtonStyle(
           backgroundColor: WidgetStateProperty.all(Colors.greenAccent)),
     );
